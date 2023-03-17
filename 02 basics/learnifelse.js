ab797970ecc3edff67b2df2b01b1ad69b885c559
unknown <arjunsinghsom50@gmail.com>
2023-03-17 11:22:12 +0530

    deleted1

--- 02 basics/learnifelse.js
+++
@@ -1,5 +0,0 @@
-if(false){
-     console.log('i am inside IF block')}
-     else{
-        console.log('i m inside ELSE block')
-     }
--- 02 basics/scope.js
+++
@@ -1,9 +0,0 @@
-let global = 'John'
-if(true){
-    let local = 'Micheal'
-    let global = 'hero'     //if we use "let" global = 'hero' then it is for its scope only but we assign value like "global"= "hero" it make it globally
-    console.log(global)
-    console.log(local)
-}
-// console.log(local) //local obj can not be used outside its lexical scope
-console.log(global)
