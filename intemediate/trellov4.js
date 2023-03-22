8b30542ce1d490553ef8221d4c6bf14cf13df522
unknown <arjunsinghsom50@gmail.com>
2023-03-22 22:36:59 +0530

    2

--- intemediate/trellov4.js
+++ intemediate/trellov4.js
@@ -1,6 +1,11 @@
 const newTodos = [{'title': 'bread'}, 
 {"title":'jam'}, {"title":'knife'},{'title' : 'go to the gym'}]
 
-const findTodo = function(todo , index){
-
+const findTodo = function(myTodo , title){
+    const index = myTodo.findIndex(function(todo , index){
+        return todo.title.toLowerCase() === title.toLowerCase()
+    })
+return myTodo[index]
 }
+
+console.log(findTodo(newTodos, 'go to the gym'))
